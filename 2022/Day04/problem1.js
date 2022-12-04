8e753febcaed2b6112e0184b520833a4f4e0c248
Jordan Ramsey <jordan.ramsey@ncino.com>
2022-12-04 14:12:36 -0500

    day 4

---
+++ 2022/Day04/problem1.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+
+let amountofPairsWithOverlap = 0;
+
+const totalScore = fs
+  .readFileSync("./2022/Day04/input.txt")
+  .toString()
+  .split("\n")
+  .forEach((elfPair) => {
+    // 2-8, 3-7;
+    let [elfOneSections, elfTwoSections] = elfPair.split(",");
+
+    let [elfOneSectionStart, elfOneSectionEnd] = elfOneSections
+      .split("-")
+      .map((elfSection) => parseInt(elfSection));
+    let [elfTwoSectionStart, elfTwoSectionEnd] = elfTwoSections
+      .split("-")
+      .map((elfSection) => parseInt(elfSection));
+
+    if (
+      (elfOneSectionStart <= elfTwoSectionStart &&
+        elfOneSectionEnd >= elfTwoSectionStart) ||
+      (elfOneSectionStart >= elfTwoSectionStart &&
+        elfOneSectionStart <= elfTwoSectionEnd)
+    ) {
+      amountofPairsWithOverlap++;
+    }
+  });
+
+console.log(amountofPairsWithOverlap);
