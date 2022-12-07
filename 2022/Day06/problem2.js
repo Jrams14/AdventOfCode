66a7635a16ff44bb51f6128bff5a792cedfd691a
Jordan Ramsey <jordan.ramsey@ncino.com>
2022-12-06 22:21:47 -0500

    day 6

---
+++ 2022/Day06/problem2.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+
+let data = fs.readFileSync("./2022/Day06/input.txt").toString();
+
+const lengthOfMarker = 14;
+
+function findMarkerIndex() {
+  for (let i = lengthOfMarker; i < data.length; i++) {
+    let markerSet = new Set([...data.slice(i - lengthOfMarker, i)]);
+    if (markerSet.size === lengthOfMarker) {
+      return i;
+    }
+  }
+}
+
+console.log(findMarkerIndex());
